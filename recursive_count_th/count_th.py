9ad521cc5cbff3e652f64df5e77ab8103f22a8f1
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-06-19 09:27:22 -0700

    Completed Count-th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,16 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
-    
-    pass
+    #If the word is less than 2 charachters it cannot contain 'th'
+    if len(word) < 2:
+        return 0
+
+    #Set count to 0
+    count = 0 
+
+    #We set it to recurse every two letters everytime it's called
+    if word[0] == 't' and word[1] == 'h':
+        count = 1
+    count = count + count_th(word[1:])
+
+    return count
